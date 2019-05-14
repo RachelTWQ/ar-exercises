4c6deacf7a5644ecbd77ffc84c4a3a4f6b6d2088
RachelTWQ <racheltang15@hotmail.com>
2019-05-14 21:18:30 +0000

    add exerices

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,9 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store2.employees.create(first_name: "ABC", last_name: "DEF", hourly_rate: 25)
+@store1.employees.create(first_name: "UVW", last_name: "XYZ", hourly_rate: 100)
+@store2.employees.create(first_name: "GHI", last_name: "JKL", hourly_rate: 45)
+@store1.employees.create(first_name: "Lol", last_name: "Lmao", hourly_rate: 80)
+@store2.employees.create(first_name: "OPQ", last_name: "RST", hourly_rate: 53)
